3d42828c581e00ec057a6deaa5e38215dc602682
Kristófer Andersen <andersen19@gmail.com>
2025-06-10 14:50:11 +0200

    removing annotations
    They don't work with minimal apis, use manual validations

--- DocumentIngestion.Api/Invoices/Dtos/CreateInvoiceRequest.cs
+++ DocumentIngestion.Api/Invoices/Dtos/CreateInvoiceRequest.cs
@@ -3,18 +3,13 @@
 namespace DocumentIngestion.Api.Invoices.Dtos;
 public class CreateInvoiceRequest
 {
-    [Required]
     public string InvoiceNumber { get; set; } = default!;
 
     public Guid SupplierId { get; set; }
 
-    [Required]
     public DateTime InvoiceDate { get; set; }
 
-    [Required]
     public string Currency { get; set; } = default!;
 
-    [Required]
-    [MinLength(1, ErrorMessage = "At least one line item required.")]
     public List<CreateInvoiceLineRequest> Lines { get; set; } = [];
 }
