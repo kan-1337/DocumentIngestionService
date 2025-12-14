5aa73fdec5af3d91ff6e036dd41c64fcdd5fe05a
Kristófer Andersen <andersen19@gmail.com>
2025-12-14 17:01:03 +0100

    const for models, keep track of status of invoice

---
+++ Frontend/src/models/invoiceConstants.ts
@@ -0,0 +1,13 @@
+export const STATUS_LABELS: Record<number, string> = {
+  0: "Not Exported",
+  1: "Exporting",
+  2: "Exported",
+  3: "Export Failed",
+};
+
+export const STATUS_COLORS: Record<number, string> = {
+  0: "status-pending",
+  1: "status-processing",
+  2: "status-success",
+  3: "status-error",
+};
