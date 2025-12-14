2a8e6b3c6d601158e39db9d96a4191c7ac0b5ae4
Kristófer Andersen <andersen19@gmail.com>
2025-12-14 20:45:56 +0100

    modify swagger for auth and create authservice config

--- Backend/DocumentIngestion.Api/Infrastructure/DependencyInjection/SwaggerConfig.cs
+++ Backend/DocumentIngestion.Api/Infrastructure/DependencyInjection/SwaggerConfig.cs
@@ -1,13 +1,52 @@
-﻿namespace DocumentIngestion.Api.Infrastructure.DependencyInjection;
+﻿using Microsoft.OpenApi.Models;
+
+namespace DocumentIngestion.Api.Infrastructure.DependencyInjection;
 public static class SwaggerConfig
 {
     public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
     {
         services.AddSwaggerGen(options =>
         {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Document Ingestion API",
+                Version = "v1"
+            });
+
+            // Add JWT Authentication
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            });
+
             var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-            options.IncludeXmlComments(xmlPath);
+            if (File.Exists(xmlPath))
+            {
+                options.IncludeXmlComments(xmlPath);
+            }
         });
         return services;
     }
